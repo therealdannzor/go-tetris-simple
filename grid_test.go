d3462e983f692bf3d2e0fb707d609f18f84a51ad
dkf <dannzor@outlook.com>
2022-12-13 14:38:12 +0000

    remove ascii pictures in test cases

--- grid_test.go
+++ grid_test.go
@@ -26,53 +26,12 @@ func TestReadShapes(t *testing.T) {
 		{"T1", "Z3", "I4"},
 		{"L0", "J3", "L5", "J8", "T1"},
 		{"L0", "J3", "L5", "J8", "T1", "T6"},
-		// 0  1  2  3  4  5  6  7  8  9
-		// .  .  .  .  .  .  .  .  .  .
-		// .  .  .  .  .  .  .  .  .  .
-		// .  .  .  .  .  .  .  .  .  .
-		// .  .  .  .  .  .  .  .  .  .
-		// .  .  .  .  .  .  .  .  .  .
-		// L  .  .  .  J  L  .  .  .  J
-		// L  T  T  T  J  L  T  T  T  J --> cleared
-		// L  L  T  J  J  L  L  T  J  J --> cleared
 		{"L0", "J3", "L5", "J8", "T1", "T6", "J2", "L6", "T0", "T7"},
-		// 0  1  2  3  4  5  6  7  8  9
-		// .  .  .  .  .  .  .  .  .  .
-		// .  .  .  .  .  .  .  .  .  .
-		// .  .  .  .  .  .  .  .  .  .
-		// .  .  .  J  .  .  L  .  .  .
-		// T  T  T  J  .  .  L  T  T  T
-		// L  T  J  J  J  L  L  L  T  J --> cleared
 		{"L0", "J3", "L5", "J8", "T1", "T6", "J2", "L6", "T0", "T7", "Q4"},
 		{"S0", "S2", "S4", "S6"},
 		{"S0", "S2", "S4", "S5", "Q8", "Q8", "Q8", "Q8", "T1", "Q1", "I0","Q4"},
-		// 0  1  2  3  4  5  6  7  8  9
-		// .  .  .  .  Q  Q  .  .  .  .
-		// I  I  I  I  Q  Q  S  S  Q  Q --> cleared
-		// .  Q  Q  .  .  S  S  .  Q  Q
-		// .  Q  Q  .  .  S  S  .  Q  Q
-		// .  T  T  T  S  S  .  .  Q  Q
-		// .  .  T  S  S  .  .  .  Q  Q
-		// .  .  S  S  .  .  .  .  Q  Q
-		// .  S  S  .  .  .  .  .  Q  Q
-		// S  S  .  .  .  .  .  .  Q  Q
 		{"L0", "J3", "L5", "J8", "T1", "T6", "S2", "Z5", "T0", "T7"},
-		// 0  1  2  3  4  5  6  7  8  9
-		// .  .  .  .  .  .  .  .  .  .
-		// .  .  .  .  .  .  .  .  .  .
-		// .  .  .  .  .  .  .  .  .  .
-		// T  T  T  S  S  Z  Z  T  T  T --> cleared
-		// L  T  S  S  J  L  Z  Z  T  J --> cleared
-		// L  T  T  T  J  L  T  T  T  J --> cleared
-		// L  L  T  J  J  L  L  T  J  J --> cleared
 		{"Q0","I2","I6","I0","I6","I6","Q2","Q4"},
-		// 0  1  2  3  4  5  6  7  8  9
-		// .  .  .  .  .  .  .  .  .  .
-		// .  .  Q  Q  .  .  .  .  .  .
-		// .  .  Q  Q  .  .  .  .  .  .
-		// I  I  I  I  Q  Q  I  I  I  I --> cleared
-		// Q  Q  .  .  Q  Q  I  I  I `I
-		// Q  Q  I  I  I  I  I  I  I  I --> cleared
 	}
 	expected := []int{1, 4, 3, 9, 0, 10, 2, 4, 1, 0, 2, 2, 2, 1, 1, 4, 3, 1, 2, 1, 8, 8, 0, 3}
 
